014b2764174cdc4ef58422daa8651f2bca1636b2
boddnej3416 <43582484+boddnej3416@users.noreply.github.com>
2018-09-25 13:10:27 -0400

    sales prediction tutorial

---
+++ P2T1_SalesPrediction_JohannesBodden.py
@@ -0,0 +1,15 @@
+# sales predictions
+# 9/25/2018
+# CTI-110 P2T1 - Sales Prediction
+# johannes bodden
+
+# get projected totalsales
+
+total_sales = float(input('Enter the projected sales: '))
+
+# calculate the profit as 23 percent of totl sales
+profit = total_sales * 0.23
+
+# Display the profit
+print('the profit is $',format(profit, ',.2f'))
+      
